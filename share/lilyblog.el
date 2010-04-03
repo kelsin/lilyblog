efa53ffd2225788d9bde20f00710a47c64162718
Christopher Giroir <kelsin@valefor.com>
2010-04-03 07:55:33 -0400

    lilyblog.el empty file added

---
+++ share/lilyblog.el
@@ -0,0 +1,31 @@
+;;; lilyblog.el --- Mode to help edit LilyBlog posts
+
+;; Copyright (C) 2010  Christopher Giroir
+
+;; Author: Christopher Giroir <cgiroir@berklee.edu>
+;; Keywords: convenience, files
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This code provides editor functions to help create and save LilyBlog blog
+;; posts. Please see github for more info
+
+;;; Code:
+
+
+
+(provide 'lilyblog)
+;;; lilyblog.el ends here
